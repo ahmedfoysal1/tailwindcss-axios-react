87ee4a64f1305a1a4c739a7189aeafd540b72b57
Foysal <ahmedmdfoysal123@gmail.com>
2024-10-28 11:53:45 +0600

    added functionality on nav toggle

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import Link from "../Link/Link";
+import { MdMenuOpen } from "react-icons/md";
+import { IoMdCloseCircle } from "react-icons/io";
 
 const Navbar = () => {
+
+    const [open, setOpen] = useState(false);
+
     const routes = [
         { id: 1, path: "/", name: "Home" },
         { id: 2, path: "/about", name: "About" },
@@ -10,13 +16,18 @@ const Navbar = () => {
       ];
       
     return (
-        <div>
+        <nav>
+            <div onClick={() => setOpen(!open)} className="md:hidden">
+                {
+                    open === true ? <IoMdCloseCircle className="text-3xl" /> : <MdMenuOpen className="text-3xl" />
+                }
+            </div>
            <ul className="md:flex">
            {
                 routes.map(route => <Link key={route.id} route={route}></Link>)
             }
            </ul>
-        </div>
+        </nav>
     );
 };
 
